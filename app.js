2dcfdf0486d637e9754b6111f303589e690231be
Davesh <singhdavesh@gmail.com>
2021-11-08 04:38:58 -0500

    input

--- app.js
+++ app.js
@@ -14,12 +14,97 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
+          },
+        {
+            type: 'input',
+            name: 'name',
             message: 'What is your name?'
         },
         {
             type: 'input',
             name: 'github',
             message: 'Enter your GitHub Username'
+            {
+                type: 'confirm',
+                name: 'confirmAbout',
+                message: 'Would you like to enter some information about yourself for an "About" section?',
+                default: true
+              },
+              {
+                type: 'input',
+                name: 'about',
+                message: 'Provide some information about yourself:',
+                when: ({ confirmAbout }) => {
+                  if (confirmAbout) {
+                    return true;
+                  } else {
+                    return false;
+                  }
+                }
+              }
+
+
         },
         {
             type: 'input',
